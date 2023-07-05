4d24b84d63dd0aaeb2d0e5d290e74dbf2e005292
Ale00coluzzi <alessandra.coluzzi00@gmail.com>
2023-07-05 22:05:22 +0200

    caricamento collaborazione fatti in presenza

---
+++ 3-homework/consigliati.php
@@ -0,0 +1,50 @@
+<?php
+    require("res/PHP/connection.php");
+
+    $connessione = new mysqli($host, $user, $password, $db);
+
+    session_start();
+
+    if(isset($_COOKIE["tema"]) && $_COOKIE["tema"] == "scuro"){
+        echo "<link rel=\"stylesheet\" href=\"res/CSS/external_con_dark.css\" type=\"text/css\" />";
+    }
+
+    else{
+        echo "<link rel=\"stylesheet\" href=\"res/CSS/external_con.css\" type=\"text/css\" />";
+    }
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <title>Recensioni... </title>
+</head>
+
+<body>
+    <h1 class="titolo">RECENSIONI&#x1F4A1;</h1>
+
+<?php
+    if(isset($_COOKIE["tema"]) && $_COOKIE["tema"] == "scuro"){
+        echo "<div class=\"home\">";
+        echo "<a href = \"homepage.php\"><img src = \"res/IMG_GIF/home2.png\" alt=\"home.png\" width=\"10%\"/></a>";
+        echo "</div>";
+    }
+    else{
+        echo "<div class=\"home\">";
+        echo "<a href = \"homepage.php\"><img src = \"res/IMG_GIF/home.png\" alt=\"home.png\" width=\"10%\"/></a>";
+        echo "</div>";
+    }
+?> 
+
+
+<div class="crediti">
+    <p>Responsabili del sito: 
+    <a href="mailto:privitera.1938225@studenti.uniroma1.it">privitera.1938225@studenti.uniroma1.it</a>    
+    <a href="mailto:coluzzi.1912970@studenti.uniroma1.it">coluzzi.1912970@studenti.uniroma1.it</a>    
+    </p>
+</div>
+
+</body>
+</html>
